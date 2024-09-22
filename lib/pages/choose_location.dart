e674b93421ed25bae1d7d074b7e5fa61f48ad4de
komxun <komxun@gmail.com>
2024-09-22 09:55:34 +0100

    added 3 pages, and route to navigate between main and choose_location

---
+++ lib/pages/choose_location.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class ChooseLocation extends StatefulWidget {
+  const ChooseLocation({super.key});
+
+  @override
+  State<ChooseLocation> createState() => _ChooseLocationState();
+}
+
+class _ChooseLocationState extends State<ChooseLocation> {
+
+  int counter = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    print('initState function ran');
+  }
+  @override
+  Widget build(BuildContext context) {
+    print('build function ran');
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Choose a Location', style: TextStyle(color: Colors.white),),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: ElevatedButton(
+        child: Text('counter is $counter'),
+        onPressed: (){
+          setState(() {
+            counter += 1;
+          });
+        },
+      ),
+    );
+  }
+}
+
+
